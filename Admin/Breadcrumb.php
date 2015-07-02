17b0ec84470a75df3b1b8e9953de3bfd316ff547
Moise Scalzo <moise.scalzo@gmail.com>
2015-07-02 20:36:07 +0200

    no message

--- Admin/Breadcrumb.php
+++ Admin/Breadcrumb.php
@@ -10,71 +10,102 @@ class Breadcrumb {
 	 */
 	public function __construct()
 	{
-		add_action( 'admin_menu', [ &$this, 'options_page' ] );
-		add_action( 'admin_init', [ &$this, 'register_fields' ] );
+		add_action( 'admin_menu', [ &$this, 'add_admin_menu' ] );
+		add_action( 'admin_init', 'settings_init' );
 	}
 
 
-	/**
-	 * Register Breadrcumb option page under the settings menu
-	 */
-	public function options_page()
+	public function add_admin_menu()
 	{
-		add_options_page( __( 'Impostazioni breadcrumb', 'kenzol' ), 'Breadcrumb', 'manage_options', 'kenzol-breadcrumb', [
-			&$this,
-			'option_page'
-		] );
+		add_options_page( __( 'Breadcrumb Settings', 'kenzol' ), 'Breadcrumb', 'manage_options', 'options-breadcrub', [ &$this, 'options_page' ] );
 	}
 
 
-	/**
-	 * Build the HTML Breadcrumb option page
-	 */
-	public function option_page()
-	{ ?>
-		<div class="wrap">
-			<h2><?= __( 'Impostazioni breadcrumb', 'kenzol' ); ?></h2>
-
-			<form method="post" action="options.php">
-				<?php
-				settings_fields( "section" );
-				do_settings_sections( "breadcrumb-options" );
-				submit_button();
-				?>
-			</form>
-		</div>
-	<?php }
-
-
-	public function post_type_elements()
+	function kenzol_settings_init()
 	{
-		$types = get_post_types( [
-			'_builtin' => false
-		] );
-		echo '<pre>'; print_r( $types ); echo '</pre>';
-		//echo '<input type="text" name="twitter_url" id="twitter_url" value="' . get_option( 'twitter_url' ) . '" />';
-	}
 
+		register_setting( 'pluginPage', 'kenzol_settings' );
 
-	/**
-	 * Register options fields
-	 */
-	public function register_fields()
-	{
-		add_settings_section( 'breadcrumb_options', 'Breadcrumb Options', null, 'kenzol-breadcrumb' );
-
-		add_settings_field(
-			'breadcrumb_options',
-			'Breadcrumb Options',
-			[ &$this, 'post_type_elements' ],
-			'kenzol-breadcrumb',
-			'ection'
+		add_settings_section(
+			'kenzol_pluginPage_section',
+			__( 'Your section description', 'kenzol' ),
+			'kenzol_settings_section_callback',
+			'pluginPage'
 		);
 
-//		add_settings_field( 'facebook_url', 'acebook Profile Url', 'isplay_facebook_element', 'heme-options', 'ection' );
-//
-//		register_setting( 'breadcrumb_options', 'breadcrumb_options' );
-//		register_setting( 'breadcrumb_options', 'breadcrumb_options' );
+		add_settings_field(
+			'kenzol_select_field_0',
+			__( 'Settings field description', 'kenzol' ),
+			'kenzol_select_field_0_render',
+			'pluginPage',
+			'kenzol_pluginPage_section'
+		);
+
+		add_settings_field(
+			'kenzol_select_field_1',
+			__( 'Settings field description', 'kenzol' ),
+			'kenzol_select_field_1_render',
+			'pluginPage',
+			'kenzol_pluginPage_section'
+		);
+
 	}
 
+
+	function kenzol_select_field_0_render()
+	{
+
+		$options = get_option( 'kenzol_settings' );
+		?>
+		<select name='kenzol_settings[kenzol_select_field_0]'>
+			<option value='1' <?php selected( $options['kenzol_select_field_0'], 1 ); ?>>Option 1</option>
+			<option value='2' <?php selected( $options['kenzol_select_field_0'], 2 ); ?>>Option 2</option>
+		</select>
+
+	<?php
+
+	}
+
+
+	function kenzol_select_field_1_render()
+	{
+
+		$options = get_option( 'kenzol_settings' );
+		?>
+		<select name='kenzol_settings[kenzol_select_field_1]'>
+			<option value='1' <?php selected( $options['kenzol_select_field_1'], 1 ); ?>>Option 1</option>
+			<option value='2' <?php selected( $options['kenzol_select_field_1'], 2 ); ?>>Option 2</option>
+		</select>
+
+	<?php
+
+	}
+
+
+	function kenzol_settings_section_callback()
+	{
+
+		echo __( 'This section description', 'kenzol' );
+
+	}
+
+
+	public function options_page()
+	{
+
+		?>
+		<form action='options.php' method='post'>
+
+			<h2><?= __( 'Breadcrumb Settings', 'kenzol' ); ?></h2>
+
+			<?php
+			settings_fields( 'pluginPage' );
+			do_settings_sections( 'pluginPage' );
+			submit_button();
+			?>
+
+		</form>
+	<?php
+
+	}
 }
